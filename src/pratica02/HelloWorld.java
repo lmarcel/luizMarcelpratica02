4a2f72abfa416d14206aad28729cef9b9815c5cb
Luiz Marcel <lmgaa@discente.ifpe.edu.br>
2021-07-01 17:32:30 -0300

    commit 02

--- src/pratica02/HelloWorld.java
+++ src/pratica02/HelloWorld.java
@@ -3,7 +3,8 @@ package pratica02;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Prática - 02 - Luiz Marcel");
+		System.out.println("Prática 02 - Luiz Marcel");
+		System.out.println("modificando o arquivo");
 
 	}
 
