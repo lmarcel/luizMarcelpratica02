17bec8dad43357c334823dbc89496269887b319d
Luiz Marcel <lmgaa@discente.ifpe.edu.br>
2021-07-07 15:49:03 -0300

    teste de commit no master

--- src/pratica02/HelloWorld.java
+++ src/pratica02/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Prática 02 - Luiz Marcel");
 		System.out.println("modificando o arquivo");
+		System.out.println("testando commit no master");
 
 	}
 
